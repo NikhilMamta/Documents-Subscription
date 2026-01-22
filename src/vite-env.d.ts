75b819c9d8b5ae52838d6c12fc0a03052bd1fbff
nikhilnishad <nikhilnishad622@gmail.com>
2026-01-22 13:41:43 +0530

    Initial commit

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+    VITE_GOOGLE_SCRIPT_URL: any
     readonly VITE_VITE_GOOGLE_SCRIPT_URL: string
     readonly VITE_GOOGLE_SHEET_ID: string
 }
